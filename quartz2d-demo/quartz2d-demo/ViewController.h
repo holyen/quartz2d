23f9d16b3f08b6d337a38eca1ca2329f83a0e39c
HolyenZou <holyenzou@gmail.com>
2013-02-01 16:43:30 +0800

    source

---
+++ quartz2d-demo/quartz2d-demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  quartz2d-demo
+//
+//  Created by holyen on 13-2-1.
+//  Copyright (c) 2013年 holyen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ quartz2d-demo/quartz2d-demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  quartz2d-demo
+//
+//  Created by holyen on 13-2-1.
+//  Copyright (c) 2013年 holyen.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
